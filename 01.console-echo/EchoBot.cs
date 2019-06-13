3d5c8d8e40464c39737b90f56e11539bb61dc95c
andrewchungxam <andrew.chung@xamarin.com>
2019-06-13 15:24:40 -0400

    Branch-1

---
+++ 01.console-echo/EchoBot.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace Console_EchoBot
+{
+    public class EchoBot : IBot
+    {
+        // Every Conversation turn for our EchoBot will call this method. In here
+        // the bot checks the <see cref="Activity"/> type to verify it's a <see cref="ActivityTypes.Message"/>
+        // message, and then echoes the user's typing back to them.
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Handle Message activity type, which is the main activity type within a conversational interface
+            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
+            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                // Echo back to the user whatever they typed.
+                await turnContext.SendActivityAsync($"You sent '{turnContext.Activity.Text}'");
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+            }
+        }
+    }
+}
---
+++ 01.console-echo/Program.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Console_EchoBot
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the EchoBot. Type something to get started.");
+
+            // Create the Console Adapter, and add Conversation State
+            // to the Bot. The Conversation State will be stored in memory.
+            var adapter = new ConsoleAdapter();
+
+            // Create the instance of our Bot.
+            var echoBot = new EchoBot();
+
+            // Connect the Console Adapter to the Bot.
+            adapter.ProcessActivityAsync(
+                async (turnContext, cancellationToken) => await echoBot.OnTurnAsync(turnContext)).Wait();
+        }
+    }
+}
